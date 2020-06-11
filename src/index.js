4b9bb76899ed27359bc01a3768a585cfd3fa034d
Rayson Kong <kong.rayson@gmail.com>
2020-06-11 09:19:38 +0800

    redux basic concepts/bare bone redux

--- src/index.js
+++ src/index.js
@@ -1,12 +1,58 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { createStore } from 'redux';
+
+// what is that Acronym for Redux? RADS
+
+// Actions
+
+const increment = () => {
+    return {
+        type: 'INCREMENT'
+    }
+}
+
+const decrement = () => {
+    return {
+        type: 'DECREMENT'
+    }
+}
+
+
+// Reducer
+
+const reducer = (state=0, action) => {
+    switch(action.type) {
+        case 'INCREMENT':
+          return state+1;
+        case 'DECREMENT':
+          return state-1;
+        default:
+          return state;
+    }
+}
+
+// create Store and loads reducer
+// This is the heart of redux 
+let store = createStore(reducer);
+
+// dispatch actions
+
+
+
+
+// Monitor
+store.subscribe(() => console.log(store.getState()));
+
+store.dispatch(increment())
+store.dispatch(increment())
+store.dispatch(increment())
+store.dispatch(decrement())
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
